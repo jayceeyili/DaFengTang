210473916860837d7b36e16670e6722330888587
Li Yi <jayceeyili@gmail.com>
2017-05-15 23:24:57 -0700

    add fakeImg for home page

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import { Image } from 'semantic-ui-react'
+import fakeImg from '../assets/image.png'
+// import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
-  </div>
+  <Image src={fakeImg} fluid />
 )
 
 export default HomeView
+
+
+// <div>
+//   <h4>Welcome!</h4>
+//   <img
+//     alt='This is a duck, because Redux!'
+//     className='duck'
+//     src={DuckImage} />
+// </div>
